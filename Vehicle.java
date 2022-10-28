4bfe7b7dc6a014e0b25bf192a26ba4f960e14709
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2022-10-28 11:42:24 +0700

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -5,19 +5,19 @@ public class Vehicle_class {
         Vehicle Ve_2 = new Vehicle();
         Vehicle Ve_3 = new Vehicle();
     //Vehicle 1
-        Ve_1.No = "1001A";
+        Ve_1.No = "V01_W1";
         Ve_1.Driver = "Varance";
         Ve_1.Quality = "good";
         Ve_1.Fuel = "Full";
         Ve_1.Location = "warehouse 1";
 //vehicle 2
-        Ve_2.No = "1002A";
+        Ve_2.No = "V02_W2";
         Ve_2.Driver = "John";
         Ve_2.Quality = "good";
         Ve_2.Fuel = "Half";
         Ve_2.Location = "warehouse 2";
 //vehicle 3
-        Ve_3.No = "1003A";
+        Ve_3.No = "V03_W2";
         Ve_3.Driver = "Jack";
         Ve_3.Quality = "bad";
         Ve_3.Fuel = "Empty";

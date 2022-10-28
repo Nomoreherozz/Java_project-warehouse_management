2c9b6da9b89c40ee5840102f133719b857970560
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2022-10-28 11:47:00 +0700

    Update Warehouse.java

--- Warehouse.java
+++ Warehouse.java
@@ -4,7 +4,7 @@ public class warehouse_class {
         warehouse Ware_1 = new warehouse();
         warehouse Ware_2 = new warehouse();
 //warehouse 1
-        Ware_1.ID ="ABCD1001"; 
+        Ware_1.ID ="W0001"; 
         Ware_1.capacity = 1000;
         Ware_1.security = "approved";
         Ware_1.fire = "approved";
@@ -12,7 +12,7 @@ public class warehouse_class {
         Ware_1.temp = 20.5;
         Ware_1.humidity = 40;
 //warehouse 2
-        Ware_2.ID ="ABCD1002"; 
+        Ware_2.ID ="W0002"; 
         Ware_2.capacity = 1200;
         Ware_2.security = "approved";
         Ware_2.fire = "disapproved";

a6d03c655bdbb0b75e5dbb140c6896a9f71ca1b7
htvvienerver <116860994+htvvienerver@users.noreply.github.com>
2022-10-29 23:25:23 +0700

    Update Warehouse.java

--- Warehouse.java
+++ Warehouse.java
@@ -10,7 +10,7 @@ public class warehouse_class {
         Ware_1.fire = "approved";
         Ware_1.light = "approved";
         Ware_1.temp = 20.5;
-        Ware_1.humidity = 40;
+        Ware_1.humidity = 10;
 //warehouse 2
         Ware_2.ID ="W0002"; 
         Ware_2.capacity = 1200;

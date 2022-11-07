459cc921132148dca8a728190640d47c18d18ea5
htvvienerver <116860994+htvvienerver@users.noreply.github.com>
2022-11-07 18:51:17 +0700

    Add files via upload

---
+++ Staff.java
@@ -0,0 +1,13 @@
+class humanity{
+    String name,job,ID,warehouse;
+    int age;
+    double salary;
+    double bonus;
+
+    void Print(){
+        System.out.print(   "Name: " + name +"\nJob: " + job + 
+                            "\nAge: " + age + "\nID: " + ID +
+                            "\nSalary: " + salary +"$" + "\nBonus: " + bonus + "$" +
+                            "\nWarehouse: " + warehouse);
+    }
+}

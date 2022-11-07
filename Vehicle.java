4a47577e6e649f75042022097ea0fe99bdf24f17
htvvienerver <116860994+htvvienerver@users.noreply.github.com>
2022-11-07 18:40:24 +0700

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -9,26 +9,6 @@ public class Vehicle_class {
         Ve_1.Driver = "Varance";
         Ve_1.Quality = "good";
         Ve_1.Fuel = "Full";
-        Ve_1.Location = "warehouse 1";
-//vehicle 2
-        Ve_2.No = "V02_W2";
-        Ve_2.Driver = "John";
-        Ve_2.Quality = "good";
-        Ve_2.Fuel = "Half";
-        Ve_2.Location = "warehouse 2";
-//vehicle 3
-        Ve_3.No = "V03_W2";
-        Ve_3.Driver = "Jack";
-        Ve_3.Quality = "bad";
-        Ve_3.Fuel = "empty";
-        Ve_3.Location = "warehouse 2";
-        
-        Ve_4.No = "V03_W2";
-        Ve_4.Driver = "Jacob";
-        Ve_4.Quality = "good";
-        Ve_4.Fuel = "full";
-        Ve_4.Location = "warehouse 1";
-        
-        
+        Ve_1.Location = "warehouse 1"; 
     }
 }

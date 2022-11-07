18b894aa27d4eafbb9950247398d3751a818fde6
htvvienerver <116860994+htvvienerver@users.noreply.github.com>
2022-11-07 18:39:33 +0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -14,31 +14,6 @@ public class Employee {
         Employee_1.salary = 1000;
         Employee_1.bonus = 300;
         Employee_1.warehouse = "warehouse 2";
-
-        Employee_2.job = "employee";
-        Employee_2.name = "Varance";
-        Employee_2.age = 40;
-        Employee_2.ID = "E002";
-        Employee_2.salary = 1000;
-        Employee_2.bonus = 300;
-        Employee_2.warehouse = "warehouse 1";
-
-        Employee_3.job = "employee";
-        Employee_3.name = "Jack";
-        Employee_3.age = 27;
-        Employee_3.ID = "E003";
-        Employee_3.salary = 1000;
-        Employee_3.bonus = 300;
-        Employee_3.warehouse = "warehouse 2";
-
-        Employee_4.job = "employee";
-        Employee_4.name = "Jacob";
-        Employee_4.age = 31;
-        Employee_4.ID = "E004";
-        Employee_4.salary = 1000;
-        Employee_4.bonus = 300;
-        Employee_4.warehouse = "warehouse 1";
-
     }
     public void isCleaning(){};
     public void isLoading(){};

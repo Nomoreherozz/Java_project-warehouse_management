cbf6feb1784011e735f90c956271134d8275ff2a
htvvienerver <116860994+htvvienerver@users.noreply.github.com>
2022-11-07 18:41:15 +0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -2,11 +2,8 @@
 public class Employee {
     public static void main(String[] args) {
 
-        humanity Employee_1 = new humanity(); 
-        humanity Employee_2 = new humanity(); 
-        humanity Employee_3 = new humanity(); 
-        humanity Employee_4 = new humanity();
-
+        humanity Employee_1 = new Staff(); 
+    
         Employee_1.job = "employee";
         Employee_1.name = "John";
         Employee_1.age = 35;

6b2275da3795a023725419002a1b63aa66e36732
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2022-11-08 22:03:23 +0700

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,13 +1,68 @@
-class Staff{
-    String name,job,ID,warehouse;
-    int age;
-    double salary;
-    double bonus;
+class Staff implements List {
+    private String name;
+    private int id;
+    private int age;
+    private double salary;
+    private Staff staffs[];
+    private int size = 0;
 
-    void Print(){
-        System.out.print(   "Name: " + name +"\nJob: " + job + 
-                            "\nAge: " + age + "\nID: " + ID +
-                            "\nSalary: " + salary +"$" + "\nBonus: " + bonus + "$" +
-                            "\nWarehouse: " + warehouse);
+    Staff(String Name, int ID, int Age, double Salary) {
+        name = Name;
+        id = ID;
+        age = Age;
+        salary = Salary;
+    }
+
+    Staff(int length) {
+        staffs = new Staff[length];
+    }
+
+    public void append(Staff staff) {
+        assert staff != null;
+        staffs[size++] = staff;
+    }
+
+    void deleteByID(int ID) {
+        for(int i = 0; i < size; i++) {
+            if(staffs[i].id == ID) {
+                staffs[i] = null;
+                staffs[i+1] = staffs[i];
+            }
+        }
+    }
+    
+    void showInfo() {
+        System.out.print(   "Name: " + name + 
+                            "\nAge: " + age + "\nID: " + id +
+                            "\nSalary: " + salary +"$");
+    }
+    
+    void searchByID(int ID) {
+        try {
+            String isExist = "NOT FOUND";
+            for(int i = 0; i < size; i++) {
+                if(staffs[i].id != ID) {
+                }
+                else {
+                    isExist = "FOUND";
+                }
+            }
+            System.out.println(isExist);
+        }
+        catch(NullPointerException ex) {}
+    }
+
+    void listShow() {
+        try {
+            for(int i = 0; i < size; i++) {
+                System.out.println(staffs[i].id);
+            }   
+        }
+        // for(int i = 0; i < size; i++) {
+        //     System.out.println(staffs[i].id);
+        // }
+        catch(NullPointerException ex) {
+
+        }   
     }
 }

08fb00193be1835fe2d070061f967c57d43456d1
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2022-11-09 11:39:57 +0700

    Update VehicleClass.java

--- VehicleClass.java
+++ VehicleClass.java
@@ -1,10 +1,12 @@
 
-class VehicleClass {
+class VehicleClass implements List {
     public String No;
     public String Driver;
     public String Quality;
     public String Fuel;
     public String Location;
+    private VehicleClass vehicles[];
+    private int size = 0;
 
     VehicleClass(String No, String Driver, String Quality, String Fuel, String Location) {
         this.No = No;
@@ -12,16 +14,64 @@ class VehicleClass {
         this.Quality = Quality;
         this.Fuel = Fuel;
         this.Location = Location;
-
+    //append
     }
-   
-    public void Print() {
+    VehicleClass(int length) {
+        vehicles = new VehicleClass[length];
+    }
+
+    public void append(VehicleClass vehicle) {
+        assert vehicle != null;
+        vehicles[size++] = vehicle;
+    }
+    //search
+    void searchByID(String NO) {
+            String isExist = "NOT FOUND";
+            for(int i = 0; i < size; i++) {
+                if(vehicles[i].No != NO || vehicles[i] == null) {
+                }
+                else {
+                    isExist = "FOUND";
+                }
+            }
+        System.out.println(isExist);
+    }
+    //show list
+    void listShow() {
+        try {
+            for (int i = 0; i < size; i++) {
+                System.out.println(vehicles[i].No);
+            }
+        } catch (NullPointerException ex) {
+
+        }
+    }
+    //delete
+    void deleteByID(String NO) {
+        for(int i = 0; i < size; i++) {
+            if(vehicles[i].No == NO) {
+                vehicles[i] = null;
+                for(int j = 0; j < size-i; j++) {
+                    vehicles[i+j] = vehicles[i+j+1];
+                    size -= 1;
+                }
+            }
+        }
+    }
+
+   //showinfo
+    public void showInfo() {
         System.out.println("Infor of Vehicle:\n No:" + No +
                 "\n Driver:" + Driver +
                 "\n Quality:" + Quality +
                 "\n Fuel:" + Fuel +
                 "\n Location:" + Location + "\n");
     }
-
+	@Override
+	public void append(Staff staff) {
+		
+		
+	}
+  
     
 }

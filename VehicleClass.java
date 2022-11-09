af8999613c28003f90a9c719a845464abf996715
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2022-11-09 10:31:22 +0700

    Update VehicleClass.java

--- VehicleClass.java
+++ VehicleClass.java
@@ -1,15 +1,27 @@
-class VehicleClass {
-    String No;
-    String Driver;
-    String Quality;
-    String Fuel;
-    String Location;
 
-    void Print() {
+class VehicleClass {
+    public String No;
+    public String Driver;
+    public String Quality;
+    public String Fuel;
+    public String Location;
+
+    VehicleClass(String No, String Driver, String Quality, String Fuel, String Location) {
+        this.No = No;
+        this.Driver = Driver;
+        this.Quality = Quality;
+        this.Fuel = Fuel;
+        this.Location = Location;
+
+    }
+   
+    public void Print() {
         System.out.println("Infor of Vehicle:\n No:" + No +
                 "\n Driver:" + Driver +
                 "\n Quality:" + Quality +
                 "\n Fuel:" + Fuel +
-                "\n Location:"+Location+"\n");
+                "\n Location:" + Location + "\n");
     }
+
+    
 }

6a885421059aa3f5d144dc24a79e29d302e5bda3
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2022-11-09 10:31:57 +0700

    Update WarehouseClass.java

--- WarehouseClass.java
+++ WarehouseClass.java
@@ -1,19 +1,30 @@
 class WarehouseClass {
-    String ID;
-    int capacity;
-    String security;
-    String fire;
-    String light;
-    double temp;
-    double humidity;
+    public String ID;
+    public int capacity;
+    public String security;
+    public String fire;
+    public String light;
+    public double temp;
+    public double humidity;
     
-    void Print() {
+    WarehouseClass(String ID, int capacity, String security, String fire, String light, double temp, double humidity) {
+        this.ID = ID;
+        this.capacity = capacity;
+        this.security = security;
+        this.fire = fire;
+        this.light = light;
+        this.temp = temp;
+        this.humidity = humidity;
+    }
+   
+    public void Print() {
         System.out.println("Infor of warehouse:\n ID:" + ID +
                 "\n capacity(in ton):" + capacity +
                 "\n security:" + security +
-                "\n Fire safety system:" + fire+
-                "\n lighting:"+light+
-                "\n Temperature(in Celcius):"+temp+
-                "\n Humidity:"+humidity+"\n");
+                "\n Fire safety system:" + fire +
+                "\n lighting:" + light +
+                "\n Temperature(in Celcius):" + temp +
+                "\n Humidity:" + humidity + "\n");
     }
+    
 }

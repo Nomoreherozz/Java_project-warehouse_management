f9e0d5af12e985c64791d951083626a93745e917
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2022-11-09 10:31:05 +0700

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
-public class Vehicle_class {
+
+public class Vehicle {
     public static void main(String[] args) {
-        Vehicle Ve_1 = new Vehicle();
-    //Vehicle 1
-        Ve_1.No = "V01_W1";
-        Ve_1.Driver = "Varance";
-        Ve_1.Quality = "good";
-        Ve_1.Fuel = "Full";
-        Ve_1.Location = "warehouse 1"; 
-    }
-    private void isRefilled();
+        VehicleClass Ve_1 = new VehicleClass("V01_W1","Varance","good","Full","warehouse 1");
+        
+
+    //Ve_1.VehicleClass("V01_W1","Varance","good","Full","warehouse 1");
     Ve_1.Print();
+    }
 }
